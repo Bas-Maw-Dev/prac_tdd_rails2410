86fa6ad2cd69b5a86c855aeeefa1697609222c05
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-10-17 10:15:21 +0100

    test: First failed test

--- spec/system/creating_blog_posts_spec.rb
+++ spec/system/creating_blog_posts_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe "CreatingBlogPosts", type: :system do
   end
 
   pending "add some scenarios (or delete) #{__FILE__}"
+  it 'saves and displays a new blog post' do
+    visit '/blog_posts/new'
+    fill_in 'Title', with: 'My first blog post'
+    fill_in 'Body', with: 'This is my first blog post'
+    click_button 'Create Blog post'
+    expect(page).to have_content('My first blog post')
+    expect(page).to have_content('This is my first blog post')
+
+    blog_post = BlogPost.order("id").last
+    expect(blog_post.title).to eq('My first blog post')
+    expect(blog_post.body).to eq('This is my first blog post')
+  end
 end
